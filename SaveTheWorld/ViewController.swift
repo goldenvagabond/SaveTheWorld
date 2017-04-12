982adb0377017cbedb22138c55990816acf23d3b
P <patrizio.pierattini@gmail.com>
2017-04-12 23:00:38 +0200

    Initial Commit

---
+++ SaveTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveTheWorld
+//
+//  Created by P on 12/04/17.
+//  Copyright © 2017 com.999games. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
